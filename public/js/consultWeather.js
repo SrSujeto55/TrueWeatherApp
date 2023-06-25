f12b05e79d55662ef11d89f6ff29d68632ebcbd8
EmilianoLP <EmilianoLP@ciencias.unam.mx>
2023-06-25 15:51:08 -0600

    proototype1

--- public/js/consultWeather.js
+++ public/js/consultWeather.js
@@ -1,10 +1,13 @@
 const error = document.querySelector('#MsjErr');
 const form = document.querySelector('#coordForm');
+
 const temp = document.querySelector('#temp');
 const humidity = document.querySelector('#humidity');
 const wind = document.querySelector('#wind');
 const clouds = document.querySelector('#clouds');
 const city = document.querySelector('#cityname');
+
+const button = document.querySelector('#reqButton');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -19,13 +22,29 @@ form.addEventListener('submit', e => {
     {
         if(data.cod != 400){
             error.textContent = '';
-            temp.textContent = 'Temperature: ' + data.temp + '°';
-            humidity.textContent = 'humidity: ' + data.humidity + '%';
-            wind.textContent = 'Wind: ' + data.wind + 'm/s';
-            clouds.textContent = 'Clouds: ' + data.clouds + '%';
-            city.textContent = 'City: ' + data.cityName;
+            temp.lastElementChild.textContent = data.temp + '°';
+            humidity.lastElementChild.textContent = data.humidity + '%';
+            wind.lastElementChild.textContent = data.wind + 'm/s';
+            clouds.lastElementChild.textContent = data.clouds + '%';
+            city.lastElementChild.textContent = data.cityName;
         }else{
             error.textContent = 'Coordenadas incorrectas, intente de nuevo';
         }
     });
+});
+
+button.addEventListener('click', () => {
+    temp.classList.add('flip');
+    setTimeout(() => {
+        humidity.classList.add('flip');
+    }, 200);
+    setTimeout(() => {
+        clouds.classList.add('flip');
+    }, 400);
+    setTimeout(() => {
+        wind.classList.add('flip');
+    }, 600);
+
 })
+
+
